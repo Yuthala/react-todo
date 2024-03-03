46ce7f2e7ac760c32c7536fb06553b7aedd82d5a
Dina Polousova <dinapolousova@gmail.com>
2024-03-03 11:22:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos ([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false
+        }
+      ])
+      setText ('');
+    } 
+  }
+
+  return (
+    <div className="App">
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)}/>
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+
+      <ul>
+        {
+          todos.map(todo => <li key={todo.id}> 
+            <input type="checkbox" />
+            <span>{todo.text}</span>
+            <span>&times;</span>
+          </li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
