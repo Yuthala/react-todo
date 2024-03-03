17a39f32491f98e12c10a276ef9ea9ec8ae97215
Dina Polousova <dinapolousova@gmail.com>
2024-03-03 20:19:09 +0300

    project without redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import {useState} from 'react';
+import TodoList from './components/TodoList';
+import InputField from './components/InputField';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
 
+  //функция добавить todo
   const addTodo = () => {
     if (text.trim().length) {
       setTodos ([
@@ -19,22 +22,35 @@ function App() {
     } 
   }
 
+  //обрабочик события onChange на input checkbox
+  const toggleTodoComplete = (todoId) => {
+    setTodos (
+      todos.map(
+        todo => {
+          if (todo.id !== todoId) return todo;
+
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+      )
+    )
+  }
+
+  //функция удалить todo
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter(todo => todo.id !== todoId))
+  }
+
   return (
     <div className="App">
-      <label>
-        <input value={text} onChange={(e) => setText(e.target.value)}/>
-        <button onClick={addTodo}>Add Todo</button>
-      </label>
-
-      <ul>
-        {
-          todos.map(todo => <li key={todo.id}> 
-            <input type="checkbox" />
-            <span>{todo.text}</span>
-            <span>&times;</span>
-          </li>)
-        }
-      </ul>
+        <InputField text={text} handleInput={setText} handleSubmit={addTodo}/>
+        <TodoList 
+          todos={todos}
+          toggleTodoComplete={toggleTodoComplete}
+          removeTodo={removeTodo}
+        />
     </div>
   );
 }
